c1bd73cdfebaa71cc758e86791e339a3a9f3ee37
PJ <demotednyk@gmail.com>
2019-09-22 22:29:52 -0400

    added prompt shuffling
     plus revisions to .js

--- source/pygmy.js
+++ source/pygmy.js
@@ -1,3 +1,5 @@
+//hi sorry this code is garbo, im in the process of making it better
+
 const journal_input = document.querySelector('textarea')
 const helper_prompt = document.getElementsByClassName('helper-prompt')[0]
 const input_controls = document.getElementsByClassName('input-controls')[0]
@@ -8,6 +10,8 @@ let ch2 = 100
 let ch3 = 150
 let ch4 = 200
 
+
+//typing actions: textarea focus, character changing
 const keyActions = function() {
   journal_input.onkeyup = function(e) {
     if (journal_input.value != 0) {
@@ -55,52 +59,93 @@ const keyActions = function() {
 }
 journal_input.addEventListener('click', keyActions())
 
+//helper prompt box toggle
 const helper_prompt_box = document.getElementsByClassName('helper-prompt-box')[0]
 const showPrompt = function() {
   helper_prompt.onmousedown = function(e) {
-     helper_prompt_box.classList.toggle('open')
-  }
-  document.getElementsByClassName('exit')[0].onmousedown = function(e) {
-    helper_prompt_box.classList.toggle('open')
-  }
+   helper_prompt_box.classList.toggle('open')
+ }
+ document.getElementsByClassName('exit')[0].onmousedown = function(e) {
+  helper_prompt_box.classList.toggle('open')
+}
 }
 
 helper_prompt.addEventListener('click', showPrompt())
 document.getElementsByClassName('exit')[0].addEventListener('click', showPrompt())
 
+//switches view mode
 const expand_switch = document.getElementsByClassName('expand')[0]
 const areaExpand = function() {
-  expand_switch.onmousedown = function(e) {
-    if (!journal_input.classList.contains('expanded')) {
-      journal_input.classList.toggle('expanded')
-      expand_switch.innerHTML = 'minimize'
-    } else {
-      journal_input.classList.toggle('expanded')
-      expand_switch.innerHTML = 'expand'
-    }
+  if (!journal_input.classList.contains('expanded')) {
+    journal_input.classList.toggle('expanded')
+    expand_switch.innerHTML = 'minimize'
+  } else {
+    journal_input.classList.toggle('expanded')
+    expand_switch.innerHTML = 'expand'
   }
 }
-expand_switch.addEventListener('click', areaExpand())
+expand_switch.addEventListener('click', function() {
+  areaExpand()
+})
 
+//mood board toggle
 const mood_board_switch = document.getElementsByClassName('mood-select')[0]
 const mood_board = document.getElementsByClassName('mood-board')[0]
 const openEmojiPanel = function() {
-  mood_board_switch.onmousedown = function(e) {
-    mood_board.classList.toggle("open")
-  }
+  mood_board.classList.toggle("open")
 }
 
-mood_board_switch.addEventListener('click', openEmojiPanel())
-
-document.addEventListener("click", function(event) {
-  if(event.target.closest(".mood-board") || event.target.closest(".mood-select")) return
-  mood_board.classList.remove("open")
+mood_board_switch.addEventListener('click', function() {
+  openEmojiPanel()
 })
 
+//mood board toggle via click off
+document.addEventListener("click", function(event) {
+  if(event.target.closest(".mood-board") || event.target.closest(".mood-select")) return
+    mood_board.classList.remove("open")
+})
+
+//mood board emoji selection
 const emojiList = document.querySelectorAll('.mood-board ul li')
 emojiList.forEach(emoji => {
   emoji.addEventListener("click", function() {    
     mood_board_switch.innerHTML = "<img src='assets/img/"+ this.querySelector("input").value +".png'>"
-        console.log(this.querySelector("input").value)
+    console.log(this.querySelector("input").value)
   })
 })
+
+//sets the time for the current entry
+const date = document.getElementById('date')
+const months = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"
+];
+const realDate = new Date()
+const setDate = function() {
+  date.innerHTML = months[(realDate.getMonth())] + " " + realDate.getDate() + " " + realDate.getFullYear()
+}
+
+setDate();
+
+//shuffles prompts
+let prompts = []
+const prompt = document.querySelector(".prompt")
+const getPrompt = function() {
+  if(prompts.length > 0) {
+    const rNum = Math.floor(Math.random() * prompts.length)
+    const rPrompt = prompts[rNum]
+    
+    prompt.innerHTML = rPrompt.prompt
+  }
+}
+//gets prompts from json file
+fetch('json/prompts.json')
+.then(response => response.json())
+.then(jsonData => {
+  prompts = jsonData
+  getPrompt()
+})
+
+document.querySelector(".new-prompt").addEventListener("click", function() {
+  getPrompt()
+})
+
