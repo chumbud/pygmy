6ba761c2df7bf94d2747807d223dfb52bc51ac48
PJ <demotednyk@gmail.com>
2020-02-01 00:58:20 -0500

    broke image retrieval for emojis woops

--- public/js/search-retrieval.js
+++ public/js/search-retrieval.js
@@ -98,7 +98,7 @@ function renderResults(a) {
 		const item = document.createElement("li")
 		item.setAttribute('data-id', entry._id)
 		item.setAttribute('data-date', entry.year + "-" + entry.month + "-" + entry.day)
-		item.innerHTML = '<img src=\"./assets/img/' + entry.selectedEmoji + '.png\"> ' + '<div class=\'search-entry-info\'>' + "<h3>" + months[entry.month-1] + ' ' + entry.day + ', ' + entry.year + '<a class="edit">edit</a><a class="delete">delete</a></h3>' + "<p class=\"entry\">" + entry.entry +"</p></div>"
+		item.innerHTML = '<img src=\"./assets/img/png/' + entry.selectedEmoji + '.png\"> ' + '<div class=\'search-entry-info\'>' + "<h3>" + months[entry.month-1] + ' ' + entry.day + ', ' + entry.year + '<a class="edit">edit</a><a class="delete">delete</a></h3>' + "<p class=\"entry\">" + entry.entry +"</p></div>"
 		item.classList.add("listEntry")
 			//for sorting after
 			resultNodes.push(item)
