45333b8f93480f8794db3e569ce67419b9ec5cca
PJ <demotednyk@gmail.com>
2020-03-01 19:56:28 -0500

    small updates
    - reworked organization of settings
    - reworked backgrounds to be more legible
    - fixed dark mode bugs
    - fixed mobile nav bug

--- public/js/mood-board.js
+++ public/js/mood-board.js
@@ -3,9 +3,9 @@ let emojiList = []
 let emojiRoster = []
 const mood_board = document.querySelector('.mood-board')
 fetch('json/emoji.json')
-.then(emojiListResponse => emojiListResponse.json())
-.then(jsonData => {
-	emojiRoster = jsonData
+  .then(emojiListResponse => emojiListResponse.json())
+  .then(jsonData => {
+    emojiRoster = jsonData
     emojiRoster.forEach(emoji => {
       //creates emoji in list element and adds it to the ul
       const option = document.createElement("li")
@@ -13,29 +13,29 @@ fetch('json/emoji.json')
       mood_board.querySelector("ul").appendChild(option)
     })
   })
-  .then(function() {
-    mood_board.addEventListener("click", function(event) {    
-      if(event.target.closest("li")) {
-      let inputEmoji = event.target.closest("li").firstChild
+  .then(function () {
+    mood_board.addEventListener("click", function (event) {
+      if (event.target.closest("li")) {
+        let inputEmoji = event.target.closest("li").firstChild
 
-      mood_board_switch.innerHTML = "<img src='assets/img/"+ inputEmoji.value +".png'>"
-      mood_board_switch.classList.add('selected')
-      //checks the emoji for submission and unchecks all others if changed
-      document.querySelectorAll('.mood-board ul li').forEach(emoji => {
-        emoji.querySelector('input').checked = false
-      })
-      inputEmoji.checked = true
+        mood_board_switch.innerHTML = "<img src='assets/img/" + inputEmoji.value + ".png'>"
+        mood_board_switch.classList.add('selected')
+        //checks the emoji for submission and unchecks all others if changed
+        document.querySelectorAll('.mood-board ul li').forEach(emoji => {
+          emoji.querySelector('input').checked = false
+        })
+        inputEmoji.checked = true
 
-      //triggers input event for search
-      var e = document.createEvent("HTMLEvents")
-      e.initEvent("input", true, true)
-      e.eventName = "input"
-      inputEmoji.dispatchEvent(e)
+        //triggers input event for search
+        var e = document.createEvent("HTMLEvents")
+        e.initEvent("input", true, true)
+        e.eventName = "input"
+        inputEmoji.dispatchEvent(e)
       }
-    })  
-	})
-  .then(function() {
-    if(sessionStorage.getItem('_id') != null) {
+    })
+  })
+  .then(function () {
+    if (sessionStorage.getItem('_id') != null) {
       hoodie.store.find(sessionStorage.getItem('_id')).then(response => {
         document.querySelector('input[value="' + response.selectedEmoji + '"]').checked = true
         console.log('input[value="' + response.selectedEmoji + '"]')
@@ -45,17 +45,17 @@ fetch('json/emoji.json')
     //im a designer, not a DeVelOpeR
     sessionStorage.removeItem("_id")
   })
-	
+
 
 
 //mood board toggle
 const mood_board_switch = document.querySelector('.mood-select')
-mood_board_switch.addEventListener('click', function() {
-	mood_board.classList.toggle("open")
+mood_board_switch.addEventListener('click', function () {
+  mood_board.classList.toggle("open")
 })
 
 //mood board toggle via click off
-document.addEventListener("click", function(event) {
-	if(event.target.closest(".mood-board") || event.target.closest(".mood-select")) return
-		mood_board.classList.remove("open")
+document.addEventListener("click", function (event) {
+  if (event.target.closest(".mood-board") || event.target.closest(".mood-select")) return
+  mood_board.classList.remove("open")
 })
--- public/js/pygmy.js
+++ public/js/pygmy.js
@@ -5,8 +5,8 @@ const input_comment = document.querySelector('.comment')
 const sayings = ['short', 'medium', 'typical', 'above average']
 const mobile = 680
 
-const keyActions = function() {
-	journal_input.onkeyup = function(e) {
+const keyActions = function () {
+	journal_input.onkeyup = function (e) {
 		if (journal_input.value.length != 0) {
 			journal_input.classList.add('focused')
 			helper_prompt.classList.add('sendToBottom')
@@ -21,11 +21,11 @@ journal_input.addEventListener('click', keyActions())
 
 //helper prompt box toggle
 const helper_prompt_box = document.getElementsByClassName('helper-prompt-box')[0]
-const showPrompt = function() {
-	helper_prompt.onmousedown = function(e) {
+const showPrompt = function () {
+	helper_prompt.onmousedown = function (e) {
 		helper_prompt_box.classList.toggle('open')
 	}
-	document.getElementsByClassName('exit')[0].onmousedown = function(e) {
+	document.getElementsByClassName('exit')[0].onmousedown = function (e) {
 		helper_prompt_box.classList.toggle('open')
 	}
 }
@@ -35,7 +35,7 @@ document.getElementsByClassName('exit')[0].addEventListener('click', showPrompt(
 
 //switches view mode
 const expand_switch = document.getElementsByClassName('expand')[0]
-expand_switch.addEventListener('click', function() {
+expand_switch.addEventListener('click', function () {
 	if (!journal_input.classList.contains('expanded')) {
 		journal_input.classList.toggle('expanded')
 		expand_switch.innerHTML = 'minimize'
@@ -48,24 +48,24 @@ expand_switch.addEventListener('click', function() {
 //shuffles prompts
 let prompts = []
 const prompt = document.querySelector(".prompt")
-const getPrompt = function() {
-	if(prompts.length > 0) {
+const getPrompt = function () {
+	if (prompts.length > 0) {
 		const rNum = Math.floor(Math.random() * prompts.length)
 		const rPrompt = prompts[rNum]
-		
+
 		prompt.innerHTML = rPrompt.prompt
 	}
 }
 
 //gets prompts from json file
 fetch('json/prompts.json')
-.then(response => response.json())
-.then(jsonData => {
-	prompts = jsonData
-	getPrompt()
-})
+	.then(response => response.json())
+	.then(jsonData => {
+		prompts = jsonData
+		getPrompt()
+	})
 
-document.querySelector(".new-prompt").addEventListener("click", function() {
+document.querySelector(".new-prompt").addEventListener("click", function () {
 	getPrompt()
 })
 
@@ -73,43 +73,43 @@ document.querySelector(".new-prompt").addEventListener("click", function() {
 let form = document.querySelector('#entry-form')
 let dateHeader = document.getElementById('date')
 let months = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December"
+	"July", "August", "September", "October", "November", "December"
 ]
 const realDate = new Date()
 const textarea = document.querySelector('textarea')
-let today = realDate.getFullYear() + "/" + (realDate.getMonth()+1) + "/" + realDate.getDate()
-const setDate = function() {
-	if(sessionStorage.getItem("date") == null) {
+let today = realDate.getFullYear() + "/" + (realDate.getMonth() + 1) + "/" + realDate.getDate()
+const setDate = function () {
+	if (sessionStorage.getItem("date") == null) {
 		dateHeader.setAttribute("data-date", today)
 		today = months[(realDate.getMonth())] + " " + realDate.getDate() + ", " + realDate.getFullYear()
 		dateHeader.innerHTML = today
 	} else {
 		dateHeader.setAttribute("data-date", sessionStorage.getItem("date"))
 		today = sessionStorage.getItem("date").split("/")
-		dateHeader.innerHTML = months[(today[1]-1)] + " " + today[2] + " " + today[0]
+		dateHeader.innerHTML = months[(today[1] - 1)] + " " + today[2] + " " + today[0]
 	}
 }
 
 //if being redirected from search, checks for an entry id for editing and turns on expanded mode
-const getAdjacentEntry = function() {
+const getAdjacentEntry = function () {
 	let prev = document.querySelector(".prev")
 	let next = document.querySelector(".next")
 
 	let entryDate = document.querySelector("#date").getAttribute("data-date").split("/")
 	entryDate = new Date([entryDate[0], entryDate[1], entryDate[2]])
-	
+
 	let prevId = null
 	let nextId = null
 	hoodie.store.findAll().then(list => {
-		let s = list.sort(function(a, b) {
-			var dateA = new Date(a.year  + "/" + a.month + "/" + a.day), dateB = new Date(b.year  + "/" + b.month + "/" + b.day)
+		let s = list.sort(function (a, b) {
+			var dateA = new Date(a.year + "/" + a.month + "/" + a.day), dateB = new Date(b.year + "/" + b.month + "/" + b.day)
 			return dateA - dateB
 		})
-		if(prev != null) {
+		if (prev != null) {
 			for (var i = s.length - 1; i >= 0; i--) {
-				let cur = new Date(s[i].year  + "/" + s[i].month + "/" + s[i].day)
+				let cur = new Date(s[i].year + "/" + s[i].month + "/" + s[i].day)
 				cur.setHours(0)
-				if(cur < entryDate) {
+				if (cur < entryDate) {
 					prevId = s[i]._id
 					prev.onclick = function listener() {
 						hoodie.store.find(prevId).then(response => {
@@ -117,22 +117,22 @@ const getAdjacentEntry = function() {
 							openInViewOnly()
 							openEntry()
 						})
-            /*
-						sessionStorage.setItem("_id", prevId)
-						window.location.replace("/")
-						*/
+						/*
+									sessionStorage.setItem("_id", prevId)
+									window.location.replace("/")
+									*/
 					}
 					prev.style.display = "inline-block"
 					break
 				}
 			}
-			if(prevId == null)
+			if (prevId == null)
 				prev.style.display = "none"
 		}
-		if(next != null) {
+		if (next != null) {
 			for (var i = 0; i <= s.length - 1; i++) {
-				let cur = new Date(s[i].year  + "/" + s[i].month + "/" + s[i].day)
-				if(cur > entryDate) {
+				let cur = new Date(s[i].year + "/" + s[i].month + "/" + s[i].day)
+				if (cur > entryDate) {
 					nextId = s[i]._id
 					next.onclick = function listener() {
 						hoodie.store.find(nextId).then(response => {
@@ -140,38 +140,38 @@ const getAdjacentEntry = function() {
 							openInViewOnly()
 							openEntry()
 						})
-            /*
-						sessionStorage.setItem("_id", nextId)
-						window.location.replace("/")
-						*/
+						/*
+									sessionStorage.setItem("_id", nextId)
+									window.location.replace("/")
+									*/
 					}
 					next.style.display = "inline-block"
 					break
 				}
 			}
-			if(nextId == null)
+			if (nextId == null)
 				next.style.display = "none"
 		}
 	}).catch(handleError)
 }
 //ensure today's entry is actually today
-document.querySelector(".latest-entry").addEventListener("click", function(e) {
+document.querySelector(".latest-entry").addEventListener("click", function (e) {
 	e.preventDefault()
-  //THIS IS BAD, FIGURE OUT BETTER IMPLEMENTATION
-  hoodie.store.findAll(function(response) {
-  	if(response.year == realDate.getFullYear() && 
-  		response.month-1 == realDate.getMonth() && 
-  		response.day == realDate.getDate()) {
-  		sessionStorage.setItem("_id", response._id)
-  }
-}).then(function() {
-	window.location.replace("/")
-})
+	//THIS IS BAD, FIGURE OUT BETTER IMPLEMENTATION
+	hoodie.store.findAll(function (response) {
+		if (response.year == realDate.getFullYear() &&
+			response.month - 1 == realDate.getMonth() &&
+			response.day == realDate.getDate()) {
+			sessionStorage.setItem("_id", response._id)
+		}
+	}).then(function () {
+		window.location.replace("/")
+	})
 })
 
 let currentSession = null
 //retrieves entry but keeps in view-only
-if(sessionStorage.getItem('_id') != null) {
+if (sessionStorage.getItem('_id') != null) {
 	hoodie.store.find(sessionStorage.getItem('_id')).then(response => {
 		getEntry(response)
 
@@ -185,23 +185,23 @@ if(sessionStorage.getItem('_id') != null) {
 		sessionStorage.removeItem("edit-mode")
 	}).catch(handleError)
 	openEntry()
-//if it's today's entry
+	//if it's today's entry
 } else {
-  //new entry for the day
-  setDate()
-  if(document.querySelector(".prev") != null || document.querySelector(".next") != null) {
-  	getAdjacentEntry()
-  }
-  
-  hoodie.store.findAll(function(response) {
-  	if(response.year == realDate.getFullYear() && 
-  		response.month-1 == realDate.getMonth() && 
-  		response.day == realDate.getDate() && sessionStorage.getItem("date") == null) {
-  		getEntry(response)
-  	openInViewOnly()
-  	openEntry()
-  }
-})
+	//new entry for the day
+	setDate()
+	if (document.querySelector(".prev") != null || document.querySelector(".next") != null) {
+		getAdjacentEntry()
+	}
+
+	hoodie.store.findAll(function (response) {
+		if (response.year == realDate.getFullYear() &&
+			response.month - 1 == realDate.getMonth() &&
+			response.day == realDate.getDate() && sessionStorage.getItem("date") == null) {
+			getEntry(response)
+			openInViewOnly()
+			openEntry()
+		}
+	})
 }
 
 //retrieves journal entry to be placed in
@@ -210,12 +210,12 @@ function getEntry(response) {
 	document.querySelector('textarea').value = response.entry
 	document.querySelector('.mood-select').innerHTML = "<img src='assets/img/" + response.selectedEmoji + ".png'>"
 	document.querySelector('.mood-select').classList.add("selected")
-	document.querySelector('h2').innerHTML = months[response.month-1] + ' ' + response.day + ', ' + response.year
+	document.querySelector('h2').innerHTML = months[response.month - 1] + ' ' + response.day + ', ' + response.year
 	document.querySelector('h2').setAttribute("data-date", response.year + "/" + response.month + "/" + response.day)
 	document.querySelector('.length-tracker').innerHTML = response.length
 	document.querySelector('.length-tracker').setAttribute('data-length-status', getComment(response.length, sessionStorage.getItem("avg")))
 
-	if(document.querySelector(".prev") != null || document.querySelector(".next") != null) {
+	if (document.querySelector(".prev") != null || document.querySelector(".next") != null) {
 		getAdjacentEntry()
 	}
 }
@@ -229,13 +229,13 @@ function openEntry() {
 }
 
 //opens in expanded view but allows for editing
-function openInViewOnly () {
+function openInViewOnly() {
 	form.classList.add("read-only")
 	document.querySelector('textarea').setAttribute("readonly", true)
 	document.querySelector('#date').innerHTML += '<a class="edit">edit</a>'
-	document.querySelector('h2 a.edit').addEventListener("click", function() {
+	document.querySelector('h2 a.edit').addEventListener("click", function () {
 		form.classList.toggle("read-only")
-		if(this.innerHTML == "edit") {
+		if (this.innerHTML == "edit") {
 			document.querySelector('.input-controls').classList.add('focused')
 			this.innerHTML = "cancel"
 		} else {
@@ -263,105 +263,105 @@ form.addEventListener("submit", (event) => {
 	let selectedEmoji = ''
 	let emojis = form.querySelectorAll('input')
 	emojis.forEach(emoji => {
-		if(emoji.checked)
+		if (emoji.checked)
 			selectedEmoji = emoji.value
 	})
 	//new session
-	if(currentSession == null) {
+	if (currentSession == null) {
 		entry = textarea.value
 		length = textarea.value.length
-		if(selectedEmoji == '')
+		if (selectedEmoji == '')
 			selectedEmoji = "empty"
 
 		let today = new Date()
-		today.setHours(0,0,0,0)
+		today.setHours(0, 0, 0, 0)
 		let submittedJournalDate = new Date(document.querySelector("#date").getAttribute("data-date"))
-		if(!entry) return
-			hoodie.store.on("add", function() { window.location.replace("/calendar.html") })
-		hoodie.store.add({selectedEmoji, entry, length, month, day, year})
+		if (!entry) return
+		hoodie.store.on("add", function () { window.location.replace("/calendar.html") })
+		hoodie.store.add({ selectedEmoji, entry, length, month, day, year })
 
 	} else {
-	//editing session
-	entry = textarea.value
-	length = textarea.value.length
+		//editing session
+		entry = textarea.value
+		length = textarea.value.length
 
-	emojis.forEach(emoji => {
-		if(emoji.checked)
-			selectedEmoji = emoji.value
-	})
-	if(!entry) return
-		hoodie.store.on("update", function() { window.location.replace("/search.html") })
-	hoodie.store.update(currentSession._id, {selectedEmoji, entry, length, month, day, year})   
-}
-sessionStorage.removeItem("date")
+		emojis.forEach(emoji => {
+			if (emoji.checked)
+				selectedEmoji = emoji.value
+		})
+		if (!entry) return
+		hoodie.store.on("update", function () { window.location.replace("/search.html") })
+		hoodie.store.update(currentSession._id, { selectedEmoji, entry, length, month, day, year })
+	}
+	sessionStorage.removeItem("date")
 })
 
 //extends comparison to Date object
-Date.prototype.isSameDateAs = function(pDate) {
+Date.prototype.isSameDateAs = function (pDate) {
 	return (
 		this.getFullYear() === pDate.getFullYear() &&
 		this.getMonth() === pDate.getMonth() &&
 		this.getDate() === pDate.getDate()
-		)
+	)
 }
 
 //gets comment for char on hover
 const getComment = function (entryLength, userLengthAvg) {
 
-	if(entryLength <= (userLengthAvg*.25))
+	if (entryLength <= (userLengthAvg * .25))
 		return sayings[0]
-	if(entryLength <= (userLengthAvg*.5))
+	if (entryLength <= (userLengthAvg * .5))
 		return sayings[1]
-	if(entryLength <= (userLengthAvg*.75))
+	if (entryLength <= (userLengthAvg * .75))
 		return sayings[2]
-	if(entryLength >= (userLengthAvg*.75))
+	if (entryLength >= (userLengthAvg * .75))
 		return sayings[3]
 	return "whoa"
 }
 
 //slight dom restructure for mobile
 let done
-window.onload = function() {
-	if(window.innerWidth <= mobile) { done = false }
-		if(window.innerWidth > mobile) { done = true }
-			mobileAdjust()
+window.onload = function () {
+	if (window.innerWidth <= mobile) { done = false }
+	if (window.innerWidth > mobile) { done = true }
+	mobileAdjust()
+}
+
+window.onresize = function () {
+	mobileAdjust()
+}
+
+function mobileAdjust() {
+	if (window.innerWidth <= mobile && !done) {
+		insertAfter(document.querySelector(".entry-header .next"), document.querySelector(".entry-header .prev"))
+
+		document.querySelector('.expand').click()
+		document.querySelector('.expand').style.display = "none"
+		textarea.classList.add('focused')
+		textarea.classList.add('expanded')
+
+		done = true
+	} else if (window.innerWidth > mobile && done) {
+
+		document.querySelector(".entry-header c:last-child").appendChild(document.querySelector(".entry-header .next"))
+		document.querySelector(".entry-header c:first-child").appendChild(document.querySelector(".entry-header .prev"))
+		document.querySelectorAll(".to-nav").remove()
+		document.querySelector('.expand').style.display = "block"
+
+		done = false
 	}
+}
 
-	window.onresize = function() {
-		mobileAdjust()
-	}
-
-	function mobileAdjust() { 
-		if(window.innerWidth <= mobile && !done) {
-			insertAfter(document.querySelector(".entry-header .next"), document.querySelector(".entry-header .prev"))
-
-			document.querySelector('.expand').click()
-			document.querySelector('.expand').style.display = "none"
-			textarea.classList.add('focused')
-			textarea.classList.add('expanded')
-
-			done = true
-		} else if(window.innerWidth > mobile && done) {
-
-			document.querySelector(".entry-header c:last-child").appendChild(document.querySelector(".entry-header .next"))
-			document.querySelector(".entry-header c:first-child").appendChild(document.querySelector(".entry-header .prev"))
-			document.querySelectorAll(".to-nav").remove()
-			document.querySelector('.expand').style.display = "block"
-
-			done = false
+function insertAfter(el, referenceNode) {
+	referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
+Element.prototype.remove = function () {
+	this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+	for (var i = this.length - 1; i >= 0; i--) {
+		if (this[i] && this[i].parentElement) {
+			this[i].parentElement.removeChild(this[i]);
 		}
 	}
-
-	function insertAfter(el, referenceNode) {
-		referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-	}
-	Element.prototype.remove = function() {
-		this.parentElement.removeChild(this);
-	}
-	NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-		for(var i = this.length - 1; i >= 0; i--) {
-			if(this[i] && this[i].parentElement) {
-				this[i].parentElement.removeChild(this[i]);
-			}
-		}
-	}
+}
